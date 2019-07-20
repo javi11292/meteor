cb45242e79fa81d254f1547924c33af27a4ce263
Javi <javi_1_12_92@hotmail.com>
2019-07-20 13:13:18 +0200

    Updates

---
+++ precache-manifest.d9e47b6791af0b655ab5a62476b24214.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7acbea5e149d6ad0ec969536ac81843",
+    "url": "/meteor/index.html"
+  },
+  {
+    "revision": "d259cfb5c2ca0c89dc9a",
+    "url": "/meteor/static/js/2.f7256afc.chunk.js"
+  },
+  {
+    "revision": "20c332718ca9f8d546a0",
+    "url": "/meteor/static/js/main.69e3dc26.chunk.js"
+  },
+  {
+    "revision": "1bd1d9e42a990a3a361f",
+    "url": "/meteor/static/js/runtime~main.c3248b4c.js"
+  }
+]);
